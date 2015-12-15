19aa431a987f8cd2e2aec0c1340a3f2c2725084e
Amir Kamran <amirkamran@msn.com>
2015-12-15 14:50:22 +0100

    test

--- src/main/java/nl/uva/illc/dataselection/InvitationModel.java
+++ src/main/java/nl/uva/illc/dataselection/InvitationModel.java
@@ -218,15 +218,15 @@ public class InvitationModel {
 			ttable[i] = new TranslationTable();
 		}
 
-		latch = new CountDownLatch(2);
+		latch = new CountDownLatch(4);
 
 		initializeTranslationTable(src_indomain, trg_indomain, ttable[0]);
 		initializeTranslationTable(trg_indomain, src_indomain, ttable[1]);
 		
 		// initialize the outdomain with normal distribution
 
-		//initializeTranslationTable(src_mixdomain, trg_mixdomain, ttable[2]);
-		//initializeTranslationTable(trg_mixdomain, src_mixdomain, ttable[3]);
+		initializeTranslationTable(src_mixdomain, trg_mixdomain, ttable[2]);
+		initializeTranslationTable(trg_mixdomain, src_mixdomain, ttable[3]);
 
 		latch.await();
 
@@ -389,8 +389,8 @@ public class InvitationModel {
 		latch = new CountDownLatch(4);
 		initializeTranslationTable(src_indomain, trg_indomain, ttable[0]);
 		initializeTranslationTable(trg_indomain, src_indomain, ttable[1]);		
-		initializeTranslationTable(src_outdomain, trg_outdomain, ttable[2]);
-		initializeTranslationTable(trg_outdomain, src_outdomain, ttable[3]);
+		initializeTranslationTable(src_mixdomain, trg_mixdomain, ttable[2]);
+		initializeTranslationTable(trg_mixdomain, src_mixdomain, ttable[3]);
 		latch.await();
 
 		for (int i = 1; i <= iMAX; i++) {
